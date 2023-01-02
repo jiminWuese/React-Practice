b20d52189c1a4e97dacc1bc498a396ac68da7410
Jimin Wuese <jiminwuese@gmail.com>
2023-01-02 10:01:15 +0000

    Update 4 files

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const Heading = () => { 
+    
+    const date = new Date();
+    console.log(date)
+    
+    return(
+        <h1 className="heading">Heading...</h1>
+    )
+}
+
+export default Heading;
